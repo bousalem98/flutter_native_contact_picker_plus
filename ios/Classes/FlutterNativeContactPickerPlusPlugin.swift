d7fcce0f067d81c657c4818b2cd2bf02110453ec
bousalem98 <hkouma2011@gmail.com>
2024-09-06 16:04:28 +0100

    fix issue

--- ios/Classes/FlutterNativeContactPickerPlusPlugin.swift
+++ ios/Classes/FlutterNativeContactPickerPlusPlugin.swift
@@ -2,14 +2,16 @@ import Flutter
 import UIKit
 import ContactsUI
 
-// Base handler class
-class PickerHandler: NSObject, CNContactPickerDelegate {
-    var result: FlutterResult
+
+
+class PickerHandler: NSObject, CNContactPickerDelegate  {
+    var result: FlutterResult;
     
     required init(result: @escaping FlutterResult) {
         self.result = result
         super.init()
     }
+    
     
     @available(iOS 9.0, *)
     public func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
@@ -17,50 +19,51 @@ class PickerHandler: NSObject, CNContactPickerDelegate {
     }
 }
 
-// Handler for single contact selection
 class SinglePickerHandler: PickerHandler {
     @available(iOS 9.0, *)
     public func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
-        var data = [String: Any]()
-        data["fullName"] = CNContactFormatter.string(from: contact, style: .fullName)
-        
-        let numbers = contact.phoneNumbers.compactMap { $0.value.stringValue }
+
+        var data = Dictionary<String, Any>()
+        data["fullName"] = CNContactFormatter.string(from: contact, style: CNContactFormatterStyle.fullName)
+
+        let numbers: Array<String> = contact.phoneNumbers.compactMap { $0.value.stringValue as String }
         data["phoneNumbers"] = numbers
-        
+
         result(data)
     }
 }
 
-// Handler for multiple contacts selection
 class MultiPickerHandler: PickerHandler {
     @available(iOS 9.0, *)
     public func contactPicker(_ picker: CNContactPickerViewController, didSelect contacts: [CNContact]) {
-        var selectedContacts = [[String: Any]]()
-        
-        for contact in contacts {
-            var contactInfo = [String: Any]()
-            contactInfo["fullName"] = CNContactFormatter.string(from: contact, style: .fullName)
-            
-            let numbers = contact.phoneNumbers.compactMap { $0.value.stringValue }
-            contactInfo["phoneNumbers"] = numbers
-            
-            selectedContacts.append(contactInfo)
-        }
-        
-        result(selectedContacts)
+        var selectedContacts = [Dictionary<String, Any>]()
+
+         for contact in contacts {
+             var contactInfo = Dictionary<String, Any>()
+             contactInfo["fullName"] = CNContactFormatter.string(from: contact, style: CNContactFormatterStyle.fullName)
+
+             let numbers: [String] = contact.phoneNumbers.compactMap { $0.value.stringValue as String }
+             contactInfo["phoneNumbers"] = numbers
+
+             selectedContacts.append(contactInfo)
+         }
+
+         result(selectedContacts)
     }
 }
 
-public class SwiftFlutterNativeContactPickerPlusPlugin: NSObject, FlutterPlugin {
-    var delegate: PickerHandler?
-    
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "flutter_native_contact_picker_plus", binaryMessenger: registrar.messenger())
-        let instance = SwiftFlutterNativeContactPickerPlusPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
-    }
 
-   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+public class SwiftFlutterNativeContactPickerPlusPlugin: NSObject, FlutterPlugin , CNContactPickerDelegate{
+    
+var _delegate: PickerHandler?;
+
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_native_contact_picker_plus", binaryMessenger: registrar.messenger())
+    let instance = SwiftFlutterNativeContactPickerPlusPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
       if("selectContact" == call.method || "selectContacts" == call.method) {
         if(_delegate != nil) {
             _delegate!.result(FlutterError(code: "multiple_requests", message: "Cancelled by a second request.", details: nil));
